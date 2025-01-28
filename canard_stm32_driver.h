e02f7b311a37e5c3242495cc54f42f1b7f3588b9
n-arms <75282394+n-arms@users.noreply.github.com>
2025-01-27 21:14:03 -0500

    Add extern C declaration for C++ compatibility

--- canard_stm32_driver.h
+++ canard_stm32_driver.h
@@ -8,8 +8,16 @@
 #ifndef INC_CANARD_STM32_DRIVER_H_
 #define INC_CANARD_STM32_DRIVER_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int16_t canardSTM32Recieve(FDCAN_HandleTypeDef *hfdcan, uint32_t RxLocation, CanardCANFrame *const rx_frame);
 int16_t canardSTM32Transmit(FDCAN_HandleTypeDef *hfdcan, const CanardCANFrame* const tx_frame);
 void getUniqueID(uint8_t id[16]);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* INC_CANARD_STM32_DRIVER_H_ */

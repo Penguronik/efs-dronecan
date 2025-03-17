bc79bfecd9cb0c3ec324519d8997579b71858d62
Teighan Miller <teighanmiller@Teighans-Laptop.local>
2025-03-17 15:00:35 -0400

    checks received messages before assigning baud rate

--- CAN_BUS_baud_detector/Core/Src/main.c
+++ CAN_BUS_baud_detector/Core/Src/main.c
@@ -274,19 +274,21 @@ uint16_t detect_baud_rate(void){
     hcan1.Init.TimeSeg1 = time_seg_1[rate];
     hcan1.Init.TimeSeg2 = time_seg_2[rate];
 
-    HAL_CAN_Start(&hcan1);
+    if (HAL_CAN_Init(&hcan1) != HAL_OK) continue;
+    if (HAL_CAN_Start(&hcan1) != HAL_OK) continue;
+    if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) continue;
 
-    if(HAL_CAN_Init(&hcan1) == HAL_OK){
-      if(HAL_CAN_Start(&hcan1) == HAL_OK){
-        if(HAL_CAN_ActiveNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) == HAL_OK){
+    uint32_t start_time = HAL_GetTick();
+    while(HAL_GetTick() - start_time < 100) {
+      if(HAL_CAN_GetRxFifoFillLevel(&hcan1, CAN_RX_FIFO0) > 0){
+        if(HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK){
           detected_rate = baud_rates[rate];
-          HAL_Delay(1000);
+          return detected_rate;
         }
       }
-
-    };
+    }
   };
-  return detected_rate;
+  return 0;
 }
 
 /* USER CODE END 4 */
